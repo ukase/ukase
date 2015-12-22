bc50c2ec33e1d1d94366ad521534f0452e335802
Pavel Uvarov <pauknone@yahoo.com>
2015-12-22 18:08:36 +0300

    [2281] Templates [2280] Html2Pdf generation

---
+++ core/src/test/java/me/entresol/ukase/web/UkaseControllerTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2015 Konstantin Lepa <konstantin+ukase@lepabox.net>
+ *
+ * This file is part of Ukase.
+ *
+ *  Ukase is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.entresol.ukase.web;
+
+import me.entresol.ukase.UkaseApplication;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@SpringApplicationConfiguration(UkaseApplication.class)
+@WebIntegrationTest(randomPort = true)
+public class UkaseControllerTest {
+    private static final String payload;
+    static {
+        try {
+            payload = getFileContent("basic-payload.json");
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot read test data");
+        }
+    }
+
+    private static String getFileContent(String fileName) throws IOException {
+        InputStream stream = UkaseControllerTest.class.getClassLoader().getResourceAsStream(fileName);
+        return StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+    }
+
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void testGenerateHtml() throws Exception {
+        String basicHtml = getFileContent("basic.html");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/html").content(payload).contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(basicHtml));
+    }
+
+    @Test
+    public void testGeneratePdf() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/pdf").content(payload).contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(this::checkPdf);
+        fail("not implemented yet");
+    }
+
+    private void checkPdf(MvcResult result) {
+        if (result.getResponse().getContentLength() <= 0) {
+            fail("Generated pdf is empty");
+        }
+    }
+}
