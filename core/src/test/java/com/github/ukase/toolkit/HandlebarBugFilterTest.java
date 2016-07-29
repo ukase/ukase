ea6a818f2a639a13262035504da937ec40af3c64
Aleksey Lvov <lvovaa@it2g.ru>
2016-07-29 18:26:32 +0300

    rename test

--- core/src/test/java/com/github/ukase/toolkit/HandlebarBugFilterTest.java
+++ core/src/test/java/com/github/ukase/toolkit/HandlebarBugFilterTest.java
@@ -2,22 +2,28 @@ package com.github.ukase.toolkit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by User on 29.07.2016.
+ * Created by alvov on 29.07.2016.
  */
 public class HandlebarBugFilterTest {
 
-    HandlebarBugFilter handlebarBugFilter = new HandlebarBugFilter();
+    private HandlebarBugFilter handlebarBugFilter;
+
+    @Before
+    public void setUp(){
+        this.handlebarBugFilter = new HandlebarBugFilter();
+    }
 
     @Test
-    public void test1() {
-        String example1 = "likglkfdsalewaj sdlfjdslkfds &amp dsasadsa &lt; asddffdshgdgfterwqq &quot dsafsafd &quot ";
+    public void simpleBugFilterTest() {
+        String example = "likglkfdsalewaj sdlfjdslkfds &amp dsasadsa &lt; asddffdshgdgfterwqq &quot dsafsafd &quot ";
 
-        String res1 = handlebarBugFilter.doFilter(example1);
+        String res = handlebarBugFilter.doFilter(example);
 
-        Assert.assertEquals(res1, "likglkfdsalewaj sdlfjdslkfds &amp; dsasadsa &lt; asddffdshgdgfterwqq &quot; dsafsafd &quot; ");
+        Assert.assertEquals(res, "likglkfdsalewaj sdlfjdslkfds &amp; dsasadsa &lt; asddffdshgdgfterwqq &quot; dsafsafd &quot; ");
     }
 }
