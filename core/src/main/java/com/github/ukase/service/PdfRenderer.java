ad0f4cf4206e21130258d41c7abf0154c4a7ddfc
Pavel Uvarov <pauknone@yahoo.com>
2018-02-13 15:02:22 +0300

    improve logging on pdf renderer

--- core/src/main/java/com/github/ukase/service/PdfRenderer.java
+++ core/src/main/java/com/github/ukase/service/PdfRenderer.java
@@ -42,12 +42,16 @@ public class PdfRenderer implements Renderer<String, byte[]> {
 
     public byte[] render(String html) throws RenderException {
         try {
+            log.debug("Start rendering pdf from html with size of " + html.length());
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             PdfSaucerRenderer renderer = provider.getRenderer(html);
+
+            log.debug("Renderer prepared");
 
             renderer.createPDF(baos);
             renderer.finishPDF();
 
+            log.debug("pdf rendered");
             return baos.toByteArray();
         } catch (IOException|DocumentException e) {
             throw new RenderException("Cannot render pdf", e, "pdf");
